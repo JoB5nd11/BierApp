f3964430ba60cef135fa163386ef5249777a6c45
Xaver115 <xavertv123@gmail.com>
2021-11-22 11:14:52 +0100

    first commit

---
+++ app/src/main/java/com/example/bierapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bierapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
